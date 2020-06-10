cf6c2e3091f87859f604696730789d927580a6b4
kaden-kykim <kaden.kykim@gmail.com>
2020-06-10 01:40:24 -0700

    Add Skeleton code for Timer MVVM

---
+++ Clock/Clock/Timer/Model/TimerModel.swift
@@ -0,0 +1,13 @@
+//
+//  Timer.swift
+//  Clock
+//
+//  Created by Kaden Kim on 2020-06-10.
+//  Copyright © 2020 CICCC. All rights reserved.
+//
+
+import Foundation
+
+struct TimerModel: Codable, Equatable {
+    
+}
